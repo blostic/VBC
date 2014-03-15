7223c68e394ecac0174aac35d8d22b147a35f79b
dextero <hukutizuviki+git@gmail.com>
2014-03-15 19:55:37 +0100

    Fix whitespaces in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,6 @@ module.exports = function (grunt) {
           '{.tmp,<%= yeoman.app %>}/scripts/{,*//*}*.js',
           '<%= yeoman.app %>/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
         ],
-      
         options: {
           livereload: true
         }
@@ -185,7 +184,7 @@ module.exports = function (grunt) {
             nodemon.on('config:update', function () {
               setTimeout(function () {
                 require('open')('http://localhost:8080/debug?port=5858');
-              }, 500);              
+              }, 500);
             });
           }
         }
