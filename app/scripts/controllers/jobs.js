2f03e787e49aa52ebb62d0f288f38a1f020f515b
blost <piotr.skibiak@gmail.com>
2014-04-04 23:38:36 +0200

    add some css to display jobs

--- app/scripts/controllers/jobs.js
+++ app/scripts/controllers/jobs.js
@@ -2,6 +2,7 @@
 
 angular.module('vcApp')
     .controller('JobsCtrl', function($scope, Auth) {
+        //$scope.code = highlight.highlightAuto("javascript", "function(a){ var x = 1; for (var i = 1; i < 5; i++) { x = x * i; } return x; }").value;
         $scope.user = Auth.user;
         $scope.test = "Test";
         Auth.show_jobs(function(res) {
