4f161b3465a385b9b50a78b013edeb16684aa91f
blost <piotr.skibiak@gmail.com>
2014-04-06 20:39:04 +0200

    add field of finished tasks

--- app/scripts/controllers/jobs.js
+++ app/scripts/controllers/jobs.js
@@ -4,9 +4,22 @@ angular.module('vcApp')
     .controller('JobsCtrl', function($scope, Auth) {
         //$scope.code = highlight.highlightAuto("javascript", "function(a){ var x = 1; for (var i = 1; i < 5; i++) { x = x * i; } return x; }").value;
         $scope.user = Auth.user;
-        $scope.test = "Test";
         Auth.show_jobs(function(res) {
-            $scope.jobs = res.usr.jobs;
+            var jobs = res.usr.jobs;
+            for (var job in jobs){
+                var finished = 0;
+                var allTasks = 0;
+                var tasks = jobs[job].tasks;
+                for (var task in tasks) {
+                    if (  tasks[task].status == "finished"){
+                        finished++;
+                    }
+                    allTasks++;
+                }
+                jobs[job].allTasks = allTasks;
+                jobs[job].finished = finished;
+            }
+            $scope.jobs = jobs;
         }, function(err) {
             alert("Fail");
 
