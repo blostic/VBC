77baa7874ad8b487f40686cd308f6e823b64585a
Bartosz Janota <b_j@bj.notebook>
2014-04-06 16:25:38 +0200

    #8 new tests

--- test/server/user/server_func_test.js
+++ test/server/user/server_func_test.js
@@ -3,19 +3,30 @@
 var should = require('should'),
 	mongoose = require('mongoose'),
 	serverFunc = require('../../../lib/models/server_function'),
-	range;
+	range,
+	primNums;
 
 
-xdescribe('Count primary numbers function test', function(){
-	before(function(done){
+describe('Count primary numbers function test', function(){
+	
+	it('should count 3, 5, 7, 11, 13 numbers', function(done){
 		range = {start:3, end: 15};
+		primNums = serverFunc(range);
+		primNums.should.have.length(5);
 		done();
 	});
 
-	it('should count 3, 5, 7, 11, 13 numbers', function(done){
-		console.log('first test');
-		primNums = serverFunc.countPrimaryNumbers(range);
-		primNums.should.have.length(6);
-		//done();
+	it('should count 1, 2, 3 numbers', function(done){
+		range = {start:1, end: 4};
+		primNums = serverFunc(range);
+		primNums.should.have.length(3);
+		done();
+	});
+
+	it('should count nothing', function(done){
+		range = {start: 4, end: 4};
+		var primNums = serverFunc(range);
+		primNums.should.have.length(0);
+		done();
 	});
 });
